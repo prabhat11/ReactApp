3900b64f5ed200fc7ca8fd9ff7c5555549a0fcaa
Prabhat11 <prabhat11.ranjan@gmail.com>
2018-02-01 22:48:59 +0530

    ajax lib added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-import {creteStore}  from 'redux'
+    import  React, {Component} from 'react';
+    import ReactDom  from 'react-dom';
 
+   class App extends Component{
+       constructor(props){
+           super()
+           this.setState ={
+               content:[]
+           }
+       }
 
-const todoreducers=(state,action) =>{
-  
-}
+       componewillmount(){
+           $.ajax({
+               
+           })
+       }
+      render(){
+             return(
+            <section>
+                <h1> Hello Prabhat !</h1> 
+                {React.Children.only(this.props.children)}
+             </section>
+    )}
+    }
+    ReactDom.render(<App>
+        <div>
+           <h1> This is firs child!  </h1>
+           <h2> This is second child </h2>
+        </div>
+         </App>,document.getElementById('root') )
---
+++ src/reducers/user-reducer.js
@@ -0,0 +1,272 @@
+[
+    {
+      "_id": "5a730a494339901d1ebe6320",
+      "index": 0,
+      "guid": "1c39ebaa-e8d5-48eb-8611-99d3e0fcaaac",
+      "isActive": true,
+      "balance": "$1,683.98",
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "brown",
+      "name": "Jimmie Simmons",
+      "gender": "female",
+      "company": "ACCUPRINT",
+      "email": "jimmiesimmons@accuprint.com",
+      "phone": "+1 (940) 414-2480",
+      "address": "903 Coleman Street, Gila, Alaska, 7556",
+      "about": "Commodo elit ipsum laborum esse sunt. Adipisicing ut magna aliqua fugiat nisi dolor dolor proident eiusmod id mollit proident. Quis dolor aute ea commodo. Ullamco adipisicing et proident cupidatat commodo ullamco adipisicing dolor enim ut est nisi et sunt. Consequat aute deserunt labore voluptate cillum ipsum tempor. Irure qui velit ex nulla.\r\n",
+      "registered": "2015-06-14T10:18:22 -06:-30",
+      "latitude": 42.356095,
+      "longitude": -37.167178,
+      "tags": [
+        "exercitation",
+        "cillum",
+        "eu",
+        "do",
+        "id",
+        "voluptate",
+        "Lorem"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Blair Underwood"
+        },
+        {
+          "id": 1,
+          "name": "Harriet Decker"
+        },
+        {
+          "id": 2,
+          "name": "Little Nelson"
+        }
+      ],
+      "greeting": "Hello, Jimmie Simmons! You have 7 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5a730a498b96350479fc298d",
+      "index": 1,
+      "guid": "8d9f3e63-3846-4135-a0dd-4e18cf654103",
+      "isActive": false,
+      "balance": "$3,490.24",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Lynn York",
+      "gender": "male",
+      "company": "BLANET",
+      "email": "lynnyork@blanet.com",
+      "phone": "+1 (822) 401-3506",
+      "address": "936 Bethel Loop, Genoa, California, 867",
+      "about": "Sint fugiat exercitation dolore ex ipsum cillum tempor eu reprehenderit aliquip. Amet voluptate irure cillum adipisicing laborum. Officia cillum enim mollit duis excepteur voluptate est. Qui dolor qui incididunt duis non culpa qui ex excepteur sint excepteur nulla reprehenderit. Et et minim sint sunt nulla. Nisi consequat cupidatat amet ipsum aliqua. Magna ad commodo dolore aute ad duis id eiusmod duis labore magna commodo.\r\n",
+      "registered": "2015-09-22T09:38:48 -06:-30",
+      "latitude": -26.70936,
+      "longitude": 128.345325,
+      "tags": [
+        "ut",
+        "amet",
+        "mollit",
+        "quis",
+        "aliquip",
+        "anim",
+        "exercitation"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Edwina Burks"
+        },
+        {
+          "id": 1,
+          "name": "Tara Owen"
+        },
+        {
+          "id": 2,
+          "name": "Beverly Gibbs"
+        }
+      ],
+      "greeting": "Hello, Lynn York! You have 4 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5a730a49f2197d38bdcfd498",
+      "index": 2,
+      "guid": "39832b6d-21a2-4295-bccd-5082dafd2d7a",
+      "isActive": false,
+      "balance": "$3,912.63",
+      "picture": "http://placehold.it/32x32",
+      "age": 30,
+      "eyeColor": "brown",
+      "name": "Aileen Carter",
+      "gender": "female",
+      "company": "ZEDALIS",
+      "email": "aileencarter@zedalis.com",
+      "phone": "+1 (948) 534-2179",
+      "address": "189 Ira Court, Trail, Arizona, 9011",
+      "about": "Excepteur sint in cillum velit ipsum ea quis aliqua fugiat. Nostrud velit sunt nostrud consectetur laboris veniam mollit ipsum tempor duis sint. Aliqua fugiat qui velit pariatur irure adipisicing magna qui consectetur consequat ex. Laborum nisi veniam esse voluptate veniam anim. Ut cillum voluptate aliqua do.\r\n",
+      "registered": "2014-10-16T09:37:11 -06:-30",
+      "latitude": -44.025773,
+      "longitude": 93.325631,
+      "tags": [
+        "pariatur",
+        "irure",
+        "sit",
+        "commodo",
+        "eu",
+        "ut",
+        "officia"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Juliet Leonard"
+        },
+        {
+          "id": 1,
+          "name": "Yvette Olsen"
+        },
+        {
+          "id": 2,
+          "name": "Jo Ashley"
+        }
+      ],
+      "greeting": "Hello, Aileen Carter! You have 10 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5a730a49d64f6abdb8cfd027",
+      "index": 3,
+      "guid": "323e762f-725f-4832-9940-76efffcd3c32",
+      "isActive": false,
+      "balance": "$3,456.73",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "brown",
+      "name": "Maxwell Wiley",
+      "gender": "male",
+      "company": "SONGLINES",
+      "email": "maxwellwiley@songlines.com",
+      "phone": "+1 (971) 502-3555",
+      "address": "741 Batchelder Street, Franklin, Alabama, 2312",
+      "about": "Reprehenderit est eu irure commodo. Elit quis ea sit labore minim ea. Reprehenderit mollit ea laborum eu. Deserunt laborum occaecat consectetur ex esse. Qui culpa laborum cupidatat pariatur deserunt deserunt ea officia duis duis excepteur fugiat ex et.\r\n",
+      "registered": "2015-06-30T05:39:43 -06:-30",
+      "latitude": 59.190299,
+      "longitude": 91.187434,
+      "tags": [
+        "commodo",
+        "deserunt",
+        "ut",
+        "fugiat",
+        "pariatur",
+        "deserunt",
+        "voluptate"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Kristina Casey"
+        },
+        {
+          "id": 1,
+          "name": "Kathy Barr"
+        },
+        {
+          "id": 2,
+          "name": "Edna Knox"
+        }
+      ],
+      "greeting": "Hello, Maxwell Wiley! You have 9 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5a730a4919ace74597798704",
+      "index": 4,
+      "guid": "031a8dac-2a5d-4051-b5e1-b7172f93ff08",
+      "isActive": false,
+      "balance": "$2,550.92",
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Elsie Ochoa",
+      "gender": "female",
+      "company": "NIXELT",
+      "email": "elsieochoa@nixelt.com",
+      "phone": "+1 (811) 588-3745",
+      "address": "186 Wilson Street, Vicksburg, Indiana, 3906",
+      "about": "Voluptate mollit minim laborum in aute elit do ut ullamco non do. Nisi id id culpa deserunt. Reprehenderit voluptate Lorem exercitation magna quis fugiat in nostrud ex ut commodo veniam occaecat. Officia minim exercitation cillum aliqua minim ut consequat aliquip sunt ipsum incididunt.\r\n",
+      "registered": "2015-12-30T01:42:23 -06:-30",
+      "latitude": -80.212147,
+      "longitude": 85.346814,
+      "tags": [
+        "qui",
+        "culpa",
+        "velit",
+        "veniam",
+        "officia",
+        "cillum",
+        "quis"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Larsen Hull"
+        },
+        {
+          "id": 1,
+          "name": "Lee Lewis"
+        },
+        {
+          "id": 2,
+          "name": "Baker Colon"
+        }
+      ],
+      "greeting": "Hello, Elsie Ochoa! You have 10 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5a730a49360161a922ed1fa8",
+      "index": 5,
+      "guid": "0fc932b8-2619-437c-a19c-3ff8f3f1dfbe",
+      "isActive": false,
+      "balance": "$1,434.70",
+      "picture": "http://placehold.it/32x32",
+      "age": 22,
+      "eyeColor": "brown",
+      "name": "Ortega Campbell",
+      "gender": "male",
+      "company": "TETAK",
+      "email": "ortegacampbell@tetak.com",
+      "phone": "+1 (935) 439-2312",
+      "address": "818 Garnet Street, Cartwright, Virgin Islands, 7624",
+      "about": "Pariatur anim ipsum deserunt proident. Exercitation qui tempor ex nulla eu reprehenderit duis amet mollit mollit Lorem quis nulla eiusmod. Culpa ipsum eiusmod eu sunt tempor aliqua excepteur tempor. Id ex excepteur duis esse labore adipisicing et. Nostrud laboris dolore eiusmod commodo quis occaecat occaecat. Ipsum in duis fugiat laboris consectetur dolore consequat aute proident sit minim non ea quis.\r\n",
+      "registered": "2014-07-14T02:24:45 -06:-30",
+      "latitude": 29.066035,
+      "longitude": -42.268231,
+      "tags": [
+        "nulla",
+        "aliquip",
+        "excepteur",
+        "ut",
+        "quis",
+        "fugiat",
+        "dolore"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Ayala Dillon"
+        },
+        {
+          "id": 1,
+          "name": "Petty Harrison"
+        },
+        {
+          "id": 2,
+          "name": "Penelope Cochran"
+        }
+      ],
+      "greeting": "Hello, Ortega Campbell! You have 2 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]
